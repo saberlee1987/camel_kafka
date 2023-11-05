0bbdbffafe8027f7323264ea8d347d914b318a75
azizi.s <azizi.s@tiddev.com>
2023-11-05 15:56:19 +0330

    add example

--- camel_kafka_producer_receiver/src/main/java/com/saber/camel_kafka_producer/dto/Person.java
+++ camel_kafka_producer_receiver/src/main/java/com/saber/camel_kafka_producer/dto/Person.java
@@ -3,15 +3,22 @@ package com.saber.camel_kafka_producer.dto;
 import com.google.gson.GsonBuilder;
 import com.google.gson.LongSerializationPolicy;
 import com.google.gson.ToNumberPolicy;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class Person {
+    @ApiModelProperty(example = "saber")
     private String firstName;
+    @ApiModelProperty(example = "azizi")
     private String lastName;
+    @ApiModelProperty(example = "0079028748")
     private String nationalCode;
+    @ApiModelProperty(example = "36")
     private Integer age;
+    @ApiModelProperty(example = "saberazizi66@yahoo.com")
     private String email;
+    @ApiModelProperty(example = "09365627895")
     private String mobile;
 
     @Override
